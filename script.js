942a8ba4ff0e65277667997d445ef6e2c0dc67bd
DnC-DEV <daniel.correa1907@hotmail.com>
2022-11-17 22:27:12 -0300

    atualização botao de menu site

--- script.js
+++ script.js
@@ -8,9 +8,18 @@ class MobileNavbar {
         this.handleClick = this.handleClick.bind(this);
     }
 
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
     handleClick() {
-        console.log(this);
-        this,this.navList.classList.toggle(this.activeClass);
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
     
     addClickEvent() {
